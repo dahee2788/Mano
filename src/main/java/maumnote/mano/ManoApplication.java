51816bf0a66bfae0f3755c9f63430fd8678173c5
dahee kim <iop6316@naver.com>
2024-08-19 18:56:20 +0900

    Initial commit

---
+++ src/main/java/maumnote/mano/ManoApplication.java
@@ -0,0 +1,13 @@
+package maumnote.mano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/maumnote/mano/ManoApplicationTests.java
@@ -0,0 +1,13 @@
+package maumnote.mano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
