d7cf5e75dbeeba8424fe909aa359bd6018665fe9
dahee kim <iop6316@naver.com>
2024-08-20 19:24:46 +0900

    저장 api 테스트 코드 추가3 고민추가

--- src/test/java/maumnote/mano/controller/NotebookControllerTest.java
+++ src/test/java/maumnote/mano/controller/NotebookControllerTest.java
@@ -48,7 +48,8 @@ class NotebookControllerTest {
                 .andExpect(status().isOk());
         String responseBody = resultActions.andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
         // then
-        // JSON으로 변환
+        // JSON으로 변환 - json으로 변환하기 위해서 @NoArgsConstructor를 붙여줬는데 테스트만을 위하여 생성자를 만드는게 맞는것일까..?
+        // ResponseNotebookDto의 경우는 builder로만 인스턴스 생성되기 원해서 생성자 어노테이션을 붙이지 않았음
         ApiResponse<ResponseNotebookDto> responseNotebookDto = objectMapper.readValue(responseBody,new TypeReference<ApiResponse<ResponseNotebookDto>>() {});
 
         // 응답 데이터 검증
