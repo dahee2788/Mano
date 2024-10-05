b7520c846af7076b24092eef531e88ed4b81afad
dahee kim <iop6316@naver.com>
2024-10-05 18:02:58 +0900

    권한 어노테이션 만들기 1. 어노테이션 생성 2. controller에 적용

--- src/main/java/maumnote/mano/controller/NoteController.java
+++ src/main/java/maumnote/mano/controller/NoteController.java
@@ -6,6 +6,7 @@ import maumnote.mano.dto.ApiResponse;
 import maumnote.mano.dto.RequestNoteDto;
 import maumnote.mano.dto.ResponseNoteDto;
 import maumnote.mano.global.ResponseMessage;
+import maumnote.mano.global.anotation.HasUserRole;
 import maumnote.mano.service.NoteService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,24 +18,28 @@ public class NoteController {
     private final NoteService noteService;
 
     @PostMapping("/note")
+    @HasUserRole
     ApiResponse<ResponseNoteDto> createNote(@RequestBody @Valid RequestNoteDto requestNoteDto) {
 
         return ApiResponse.response(HttpStatus.OK.value(), ResponseMessage.NOTE_CREATE_SUCCESS, noteService.createNote(requestNoteDto));
     }
 
     @PatchMapping("/note/{noteId}")
+    @HasUserRole
     ApiResponse<ResponseNoteDto> updateNote(@PathVariable("noteId") long noteId, @RequestBody @Valid RequestNoteDto requestNoteDto) {
 
         return ApiResponse.response(HttpStatus.OK.value(), ResponseMessage.NOTE_UPDATE_SUCCESS, noteService.updateNote(noteId, requestNoteDto));
     }
 
     @GetMapping("/note/{noteId}")
+    @HasUserRole
     ApiResponse<ResponseNoteDto> getNote(@PathVariable("noteId") long noteId) {
 
         return ApiResponse.response(HttpStatus.OK.value(), ResponseMessage.SUCCESS, noteService.findNote(noteId));
     }
 
     @DeleteMapping("/note/{noteId}")
+    @HasUserRole
     ApiResponse<Boolean> deleteNote(@PathVariable("noteId") long noteId) {
         return ApiResponse.response(HttpStatus.OK.value(), ResponseMessage.SUCCESS, noteService.deleteNote(noteId));
     }
