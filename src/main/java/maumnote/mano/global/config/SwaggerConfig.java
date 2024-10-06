42ff315cdf48b20d5da45f6bdc30582464e9ce47
dahee kim <iop6316@naver.com>
2024-10-06 22:04:54 +0900

    swagger 추가 login endpoint 추가

--- src/main/java/maumnote/mano/global/config/SwaggerConfig.java
+++ src/main/java/maumnote/mano/global/config/SwaggerConfig.java
@@ -1,54 +1,95 @@
 package maumnote.mano.global.config;
 
+
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.customizers.OpenApiCustomiser;
+import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+
+        SecurityScheme apiKey = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .bearerFormat("JWT")
+                .scheme("bearer")
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Bearer Token");
+
         return new OpenAPI()
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth")) // API에서 인증 요구
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes("bearerAuth",  // "bearerAuth"라는 인증 스키마 정의
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)  // HTTP 방식 인증
-                                        .scheme("bearer")                // Bearer 토큰 방식
-                                        .bearerFormat("JWT")))
+                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+                .addSecurityItem(securityRequirement)
+                .tags(List.of(
+                        new Tag()
+                                .name("Authentication")
+                                .description("Login/logout controller")
+                ))
+//                .path("/logout", new PathItem()
+//                        .post(new Operation()
+//                                .tags(List.of(
+//                                        "Authentication"
+//                                ))
+//                                .summary("Logout")
+//                                .description("Logout the current user.")
+//                                .operationId("logout")
+//                                .responses(new ApiResponses()
+//                                        .addApiResponse("200", new ApiResponse().description("OK"))
+//                                )
+//                        )
+//                )
+                .path("/login", new PathItem()
+                        .post(new Operation()
+                                .tags(List.of(
+                                        "Authentication"
+                                ))
+                                .summary("Login")
+                                .description("응답받은 access token을 'Bearer + token' 방식으로 Authorize에 입력해주세요.")
+                                .operationId("login")
+                                .requestBody(new RequestBody()
+                                        .content(new Content()
+                                                .addMediaType("application/json", new MediaType()
+                                                        .schema(new Schema()
+                                                                .addProperty("email", new Schema().type("string"))
+                                                                .addProperty("password", new Schema().type("string"))
+                                                        )
+                                                )
+                                        )
+                                )
+                                .responses(new ApiResponses()
+                                        .addApiResponse("200", new ApiResponse().description("OK"))
+                                        .addApiResponse("401", new ApiResponse().description("이메일과 비밀번호를 확인해주세요."))
+                                )
+                        )
+                )
                 .info(apiInfo());
-    }
-
-    // todo : /login endpoint 노출안되고 있음. 
-    @Bean
-    public OpenApiCustomiser loginEndpointCustomizer() {
-        return openApi -> {
-            // /login 엔드포인트에 POST 요청을 정의
-            Operation loginOperation = new Operation()
-                    .summary("User Login")  // 요약 정보
-                    .description("API to login users");  // 상세 설명
-
-            // PathItem에 POST 메소드와 위에서 정의한 operation 추가
-            PathItem loginPathItem = new PathItem()
-                    .post(loginOperation);
-
-            // openApi 문서의 paths에 /login 엔드포인트 추가
-            openApi.getPaths().addPathItem("/login", loginPathItem);
-        };
     }
 
     private Info apiInfo() {
         return new Info()
                 .title("MaumNote : Mano")
-                .description("하루의 기분을 작성할 수 있는 일기 Api 입니다.")
-                .version("2.0");
+                .description("하루의 기분을 작성할 수 있는 일기 Api 입니다.\n\n 로그인부터 하시어 응답받은 token으로 인증을 먼저 진행해주세요.")
+                .version("1.0");
     }
 
 
