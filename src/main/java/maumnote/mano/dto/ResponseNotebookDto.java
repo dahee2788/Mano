c4f53d1f9d1193b5806e7b4150ed9e35d7a20f8b
dahee kim <iop6316@naver.com>
2024-10-08 00:00:43 +0900

    swagger 추가 -dto description 추가

--- src/main/java/maumnote/mano/dto/ResponseNotebookDto.java
+++ src/main/java/maumnote/mano/dto/ResponseNotebookDto.java
@@ -1,16 +1,20 @@
 package maumnote.mano.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
-import maumnote.mano.domain.BaseEntity;
 import lombok.experimental.SuperBuilder;
+import maumnote.mano.domain.BaseEntity;
 
+@Schema(description = "일기장 저장 응답 DTO")
 @Getter
 @SuperBuilder
 @ToString(callSuper = true)
 public class ResponseNotebookDto extends BaseEntity {
+    @Schema(description = "일기장 id , 일기장의 고유키입니다.", example = "1234")
     private Long id;
+
+    @Schema(description = "일기장 이름", example = "일기장")
     private String name;
 
 }
